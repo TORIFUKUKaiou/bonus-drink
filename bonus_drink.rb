e7bbab6058a04535ee8865ad9faeb8fb52a3c676
Awesome YAMAUCHI <torifuku.kaiou@gmail.com>
2025-11-21 11:33:52 +0900

    hoge hoge fuga fuga

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    if amount.zero?
+      0
+    elsif amount.odd?
+      3 * amount / 2
+    elsif amount.even?
+      3 * (amount - 1 ) / 2 + 1
+    end
   end
 end
